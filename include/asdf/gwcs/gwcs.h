299820464ef61dd263f3ba9432b276f5d1fd7479
E. Madison Bray <676149+embray@users.noreply.github.com>
2026-05-11 11:48:47 +0200

    feat: basic evaluation backend interface
    to be fleshed out more later, but the idea is allow a registry of
    different evaluation backends -- for now only the AST+YamlChan backend
    is provided built-in but opens up other possibilities in the future,
    including support for backends provided by plugins (I have more ideas in
    mind for built-in support as well...)

--- include/asdf/gwcs/gwcs.h
+++ include/asdf/gwcs/gwcs.h
@@ -4,6 +4,7 @@
 #ifndef ASDF_GWCS_GWCS_H
 #define ASDF_GWCS_GWCS_H
 
+#include <asdf/gwcs/backend.h>
 #include <asdf/gwcs/core.h>
 #include <asdf/gwcs/fitswcs_imaging.h>
 #include <asdf/gwcs/frame2d.h>
